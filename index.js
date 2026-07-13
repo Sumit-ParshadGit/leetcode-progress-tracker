b48b5356a27874419af8c0f3e425fba0361e9cd7
Sumit Parshad <sumit.sp776@gmail.com>
2026-07-14 00:50:12 +0530

    Fix total solved count calculation

--- index.js
+++ index.js
@@ -97,13 +97,13 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("hardSolvedCard").textContent = hardSolved;
         document.getElementById("hardSubmissionCard").textContent = parsedData.totalSubmissions[3].submissions;
 
-        document.getElementById("totalSolvedCard").textContent =
-        parsedData.totalSubmissions[3].submissions +
-        parsedData.totalSubmissions[2].submissions +
-        parsedData.totalSubmissions[1].submissions;
+        document.getElementById("totalSolvedCard").textContent = parsedData.totalSolved
+        
 
         document.getElementById("totalQuestionCard").textContent = totalQuestions;
-        document.getElementById("totalSubmissionCard").textContent = parsedData.totalSolved;
+        document.getElementById("totalSubmissionCard").textContent = parsedData.totalSubmissions[3].submissions +
+        parsedData.totalSubmissions[2].submissions +
+        parsedData.totalSubmissions[1].submissions;
         updateProgress(
             easySolved,
             totalEasy,
